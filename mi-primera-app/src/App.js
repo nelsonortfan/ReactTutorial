616ea4a715b3a4d848c8c81bfb3d8469868e5490
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-24 21:29:46 -0500

    front para visualizar nombre e imagen desde el backend

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+function App() 
+{
+  const [datos, setDatos] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/users/Pedro/details') // Ajusta la URL a tu endpoint
+      .then(response => setDatos(response.data))
+      .catch(error => console.error('Error al obtener datos:', error));
+  }, []);
+
   return (
-    <div>
-      <h1>¡Hola, mundo!</h1>
-      <p>Mi primera app en React</p>
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      {datos ? (
+        <>
+          <h1>{datos.name}</h1>
+          <img
+            src={`data:image/jpeg;base64,${datos.pictureBase64}`}
+            alt="Imagen del usuario"
+            style={{ width: '300px', borderRadius: '8px' }}
+          />
+        </>
+      ) : (
+        <p>Cargando datos...</p>
+      )}
     </div>
   );
-
 }
 
 export default App;
