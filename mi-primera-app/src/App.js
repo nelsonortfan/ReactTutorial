d4f84ea5c7232edb053cf73b14e0389a1539df50
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-24 22:09:24 -0500

    Agregando Spinner y carga de la foto actualizada

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Spinner.css'
 
 function App() 
 {
   const [datos, setDatos] = useState(null);
   const [nuevaImagen, setNuevaImagen] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [imagenActualizada, setImagenActualizada] = useState(false);
+
+
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/users/Pedro/details') // Ajusta la URL a tu endpoint
       .then(response => setDatos(response.data))
       .catch(error => console.error('Error al obtener datos:', error));
-  }, []);
+  }, [imagenActualizada]);
 
   const handleImagenChange = (e) => {
     const file = e.target.files[0];
@@ -25,12 +30,26 @@ function App()
   const actualizarImagen = () => {
     if (!nuevaImagen) return;
 
+    setLoading(true);
     axios.put('http://localhost:8080/api/users/api/usuario/imagen', {
       nombre: datos.name,
       imagenBase64: nuevaImagen
     })
-    .then(() => alert('Imagen actualizada correctamente'))
-    .catch(err => console.error('Error al actualizar imagen:', err));
+
+    .then(() => {
+      setTimeout(() => {
+        setLoading(false);
+        if (window.confirm('Imagen actualizada correctamente. ¿Deseas verla ahora?')) {
+          setImagenActualizada(prev => !prev); // fuerza recarga de datos
+        }
+      }, 1000); // simula tiempo de carga
+    })
+    .catch(err => {
+      setLoading(false);
+      console.error('Error al actualizar imagen:', err);
+    });
+
+
   };
 
   return (
@@ -40,15 +59,18 @@ function App()
           <h1>{datos.name}</h1>
           <img
             src={`data:image/jpeg;base64,${datos.pictureBase64}`}
-            alt="Imagen del usuario"
+            alt="Imagen actual"
             style={{ width: '300px', borderRadius: '8px' }}
           />
+
           <div style={{ marginTop: '1rem' }}>
-            <input type="file" accept="image/*" onChange={handleImagenChange} />
-            <button onClick={actualizarImagen} style={{ marginLeft: '1rem' }}>
+            <input type="file" accept="image/*" onChange={handleImagenChange} disabled={loading} />
+            <button onClick={actualizarImagen} disabled={loading} style={{ marginLeft: '1rem' }}>
               Actualizar imagen
             </button>
           </div>
+          {loading && <div className="spinner"></div>}
+        
         </>
       ) : (
         <p>Cargando datos...</p>
