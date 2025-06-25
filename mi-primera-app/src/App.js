be6761dfd8f1a768af1180c4b51d550b49ce91f6
nelsonortfan <n.ortizf@uniandes.edu.co>
2025-06-24 21:01:02 -0500

    Primer App en React ejecutandose

---
+++ mi-primera-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, mundo!</h1>
+      <p>Mi primera app en React</p>
+    </div>
+  );
+
+}
+
+export default App;
